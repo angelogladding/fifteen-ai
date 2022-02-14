e848ba50e348bc161a923938725062a48cbb782f
Angelo Gladding <angelo@lahacker.net>
2022-02-13 18:41:35 -0800

    Allow to skip save and always return API response

--- fifteen_ai/__init__.py
+++ fifteen_ai/__init__.py
@@ -15,18 +15,25 @@ class CharacterNotFound(Exception):
 
 
 def tts(text, character="GLaDOS", emotion="Contextual", filename="speech.wav"):
-    """Generate speech from `text` using `character` with `emotion`."""
+    """
+    Generate speech from `text` using `character` with `emotion`.
+
+    Do not save audio file if `filename` is None.
+
+    """
     if len(text) > max_text_len:
         logging.warning(f"text longer than {max_text_len} characters; trimming")
         text = text[:max_text_len]
     if not text.endswith((".", "!", "?")):
         text += "."
-    filename = pathlib.Path(filename)
     generation = requests.post(
         api_endpoint, json={"text": text, "character": character, "emotion": emotion}
     )
     if generation.status_code == 422:
         raise CharacterNotFound(character)
-    audio = requests.get(cdn_endpoint + generation.json()["wavNames"][0])
-    with filename.open("wb") as fp:
-        fp.write(audio.content)
+    response = generation.json()
+    if filename:
+        audio_data = requests.get(cdn_endpoint + response["wavNames"][0])
+        with pathlib.Path(filename).open("wb") as fp:
+            fp.write(audio_data.content)
+    return response
